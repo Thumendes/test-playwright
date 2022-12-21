fd8ba052d6ddce0d2a3659ef46dc14d79a9b331e
Arthur Mendes <thumendess@gmail.com>
2022-12-21 14:06:19 -0300

    Adicionando servidor HTTP

--- index.ts
+++ index.ts
@@ -1,16 +1,34 @@
-import { firefox } from "playwright";
+import { Browser, firefox } from "playwright";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
-async function main() {
-  const browser = await firefox.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+let browser: Browser | undefined;
 
-  await page.goto("https://greensignal.com.br/");
+async function getPageTitle(url: string) {
+  if (!browser) browser = await firefox.launch({ headless: false });
 
+  const page = await browser.newPage();
+  await page.goto(url);
   const title = await page.title();
-  console.log(title);
+  await page.close();
+  return title;
 }
 
-main()
-  .catch(console.error)
-  .then(() => process.exit(0));
+async function main() {
+  const app = express();
+  app.use(cors());
+  app.use(morgan("dev"));
+
+  app.get("/", async (req, res) => {
+    const title = await getPageTitle("https://www.google.com");
+
+    res.send(`O título da página é: ${title}`);
+  });
+
+  app.listen(3000, () => {
+    console.log("🔥 Server running on port 3000");
+  });
+}
+
+main().catch(console.error);
